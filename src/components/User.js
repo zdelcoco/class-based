0bc06b4e6f3c6d598f5cb4020a236989e4aeb21b
Zachary Delcoco <zdelcoco@gmail.com>
2023-06-18 21:26:21 -0500

    end of module

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,19 @@
+import { Component } from 'react';
+
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
+class User extends Component {
+  componentWillUnmount() {
+    console.log('unmounted');
+  };
+
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  };
 };
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
